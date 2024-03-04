7931871643523a1f17efaf7c79a86e6941adab8f
cromanalcaidev <cromanalcaidev@gmail.com>
2024-03-04 23:22:49 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Carlos Román Alcaide on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Carlos Román Alcaide on 4/3/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    ] //añadimos columnas
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+            
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
+
+
+
+struct FrameworkTitleView: View {
+    
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
