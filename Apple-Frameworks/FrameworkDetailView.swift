739f5831c9b81c7363d33c950e1377c90998aa57
cromanalcaidev <cromanalcaidev@gmail.com>
2024-03-05 22:37:25 +0100

    Finished the grid Looped through the framework to programatically add all the elements Added additional padding to icons polished the code

---
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Carlos Román Alcaide on 5/3/24.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
