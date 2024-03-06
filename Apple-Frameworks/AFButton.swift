a498d635d02daedf25eebc6b1ef3a1c738465630
cromanalcaidev <cromanalcaidev@gmail.com>
2024-03-06 22:57:36 +0100

    App finished Created single view Added modal

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,18 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by Carlos Román Alcaide on 6/3/24.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AFButton()
+}
---
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by Carlos Román Alcaide on 6/3/24.
+//
+
+import Foundation
